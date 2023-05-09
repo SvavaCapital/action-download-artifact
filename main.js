c8187762fcc34ff4c112df0efc2a90610945d0f7
Shantanu Tripathi <shantanu@syfe.com>
2023-05-09 15:05:59 +0530

    revert last commit

--- main.js
+++ main.js
@@ -76,16 +76,12 @@ async function main() {
 
         if (pr) {
             core.info(`==> PR: ${pr}`)
-            /*
-              Commenting the below change in order to not override the value of input variable commit
-              if its passed already (same as branch). 
-            */
-            // const pull = await client.rest.pulls.get({
-            //     owner: owner,
-            //     repo: repo,
-            //     pull_number: pr,
-            // })
-            // commit = pull.data.head.sha
+            const pull = await client.rest.pulls.get({
+                owner: owner,
+                repo: repo,
+                pull_number: pr,
+            })
+            commit = pull.data.head.sha
             //branch = pull.data.head.ref
         }
 
